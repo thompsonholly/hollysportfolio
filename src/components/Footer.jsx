056b4fba7f88d5527efef277fe29c5767e6c9d24
Holly T <144973465+thompsonholly@users.noreply.github.com>
2024-02-08 05:03:52 -0600

    Fixing contact page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
   return (
     <footer className="w-100 mt-auto text-dark p-4">
       <div id="footer">
-        <div className="container text-center mb-5">
+        <div className="footer-container text-center mb-5">
           {location.pathname !== '/' && (
             <button
               className="btn btn-dark mb-3"
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,17 +3,17 @@ import '../App.css'
 
 function Contact() {
   return (
-    <div>
+    <div className='contact-container'>
       <h1>Contact Me</h1>
 
       <h3> Email: </h3>
-      <a href="mailto:hollythompson1518@gmail.com">Click to Email Me</a>
+      <a className='active-links align-content-center' href="mailto:hollythompson1518@gmail.com">Click to Email Me</a>
 
       <h3>LinkedIn Profile:</h3>
-      <a href="https://www.linkedin.com/in/holly-thompson1518/" target="_blank">LinkedIn</a>
+      <a className='active-links' href="https://www.linkedin.com/in/holly-thompson1518/" target="_blank">LinkedIn</a>
 
       <h3>Github Profile:</h3>
-      <a href="https://github.com/thompsonholly" target="_blank">Github Link</a>
+      <a className='active-links' href="https://github.com/thompsonholly" target="_blank">Github Link</a>
 
     </div>
 
